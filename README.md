7694d23797b3ad18bf731b11a7bfa283a1e52869
juan-bolivar <j_bolivar@outlook.com>
2023-01-28 01:32:10 +0000

    Added installation of lugo

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
-# General Purpose IaC - for personal Services
+# Personal Webpage using Lugo in Digital Ocean
 
 ## Webpage
 
-Creates personal webpage using apache and terraform with the terraform provider.
+Creates a [Lugo](https://github.com/LukeSmithxyz/lugo) webpage using Apache and Terraform with the digital ocean provider.
 
-Needs the following:
+### Requisites:
+- digital-ocean-token: [Documentation ](https://docs.digitalocean.com/reference/api/create-personal-access-token/)
+- FQDN registered with digital ocean nameservers: [ns registration](https://docs.digitalocean.com/tutorials/dns-registrars/)
 
-config.ini file with :
+### Use
 
-- do_token
-- pvt_key path
+config.ini file with:
+
+- do_token.
+- pvt_key path.
 - servername.
 
+For creation:
 
+`bash start.sh`
 
+For deletion:
+
+`bash destroy.sh`
--- start.sh
+++ start.sh
@@ -3,24 +3,38 @@
 source config.ini
 
 function terraform_run(){
-
-	cd terraform-webpage
+	pushd terraform
 	terraform init 
-	terraform plan -var "do_region=$do_region" -var "server=$server" -var "do_token=$do_token" -var "pvt_key=$pvt_key" 
-	terraform apply -var "do_region=$do_region" -var "server=$server" -var "do_token=$do_token" -var "pvt_key=$pvt_key" 
+	terraform plan  -var "do_region=$do_region" -var "server=$server" -var "do_token=$do_token" -var "pvt_key=$pvt_key" 
+	terraform apply   -var "do_region=$do_region" -var "server=$server" -var "do_token=$do_token" -var "pvt_key=$pvt_key" 
 	terraform output > output.txt
-	cd ..
+	popd
+}
+
+
+check_dns(){
+
+	ipv4_webpage="$(cat terraform/output.txt | sed 's/ipv4_webpage = //' | sed 's/"//g')" 
+	DNS_IP_OBJECTIVE=$ipv4_webpage
+	DNS_IP=""
+	COUNTER=0
+
+	while [  "$DNS_IP" !=  "$DNS_IP_OBJECTIVE" ] && [ $COUNTER -lt 300 ]; do
+  		DNS_IP=$(nslookup $1 | awk '/^Address: / { print $2 }')
+		echo "$DNS_IP"
+		echo "$DNS_IP_OBJECTIVE"
+  		sleep 1
+  		((COUNTER++))
+		echo "Waiting for DNS to be available $COUNTER seconds"
+	done
+	[  "$DNS_IP" != "$DNS_IP_OBJECTIVE" ] && echo "timeout" || echo $DNS_IP 
 }
 
 function ansible_run(){
-	cd terraform-webpage 
-	ipv4_webpage="$(cat output.txt | sed 's/ipv4_webpage = //' | sed 's/"//g')" 
-	echo "$ipv4_webpage"
-	cd .. && cd webpage
-	sleep 120
-	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i "$ipv4_webpage," -e "server=$server"  main.yaml
-	cd ..
+	ipv4_webpage="$(cat terraform/output.txt | sed 's/ipv4_webpage = //' | sed 's/"//g')" 
+	ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i "$ipv4_webpage," -e "server=$server"  ansible/main.yaml
 }
 
 terraform_run
+check_dns $server
 ansible_run
